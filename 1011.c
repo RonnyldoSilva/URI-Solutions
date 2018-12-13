9673a5fa1707c2f7aed101b43ea736816cbb84ad
Ronnyldo Silva <37785171+RonnyldoSilva@users.noreply.github.com>
2018-12-13 09:12:52 -0200

    Solution.

---
+++ 1011.c
@@ -0,0 +1,13 @@
+<include <stdio.c>
+
+int main()
+{
+  double R, pi, VOLUME;
+  pi = 3.141559;
+  
+  scanf("lf", R);
+  
+  VOLUME = (4.0/3,0) * pi * R * R * R;
+  
+  printf("VOLUME = %.3lf\n", VOLUME);
+}
