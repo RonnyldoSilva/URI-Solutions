55b7fba81d94a7026157f74440a1ed29429fce84
Ronnyldo Silva <37785171+RonnyldoSilva@users.noreply.github.com>
2018-12-16 23:41:35 -0200

    Create 2137.c

---
+++ 2137.c
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)(x.size()))
+#define ii pair<int,int>
+#define vii vector<ii>
+#define vi vector<int>
+//#define int long long
+#define EPS 0.00001
+#define oo 1000000005
+
+main()
+{
+  int n;
+
+  while( cin >> n )
+  {
+    string vetor[1005];
+    for(int i = 0; i < n; i++)
+    {
+      cin >> vetor[i];
+    }
+    sort(vetor, vetor + n);
+
+    for(int i = 0; i < n; i++)
+    {
+      cout << vetor[i]  << endl ;
+    {
+  }
+}
